3fe7c2c3653aab156fc8d0d9093eb0ac322e50a6
Mateusz Gumienny <mateusznr7@gmail.com>
2025-10-23 16:23:20 +0200

    Add bonus files for get_next_line implementation; include utility functions and header

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgumienn <mgumienn@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 16:25:24 by mgumienn          #+#    #+#             */
+/*   Updated: 2025/10/19 19:02:55 by mgumienn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdint.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+#ifndef BUFFER_SIZE 
+# define BUFFER_SIZE 1
+#endif
+
+char	*get_next_line(int fd);
+int		ft_strchr(char *str, int c);
+size_t	ft_strlcpy(char **dest, char *src, size_t size);
+char	*ft_strjoin(char *s1, char c);
+char	*ft_strdup(char *src);
+
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgumienn <mgumienn@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 18:16:47 by mgumienn          #+#    #+#             */
+/*   Updated: 2025/10/19 17:18:26 by mgumienn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	dup = malloc(ft_strlen(src) * sizeof(char) + 1);
+	if (dup == NULL)
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+int	ft_strchr(char *str, int c)
+{
+	if (c == 0 || !str)
+		return (0);
+	while (*str)
+	{
+		if (*str == (char) c)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char c)
+{
+	char	*str;
+	size_t	i;
+	size_t	s1_len;
+
+	i = 0;
+	if (!s1)
+	{
+		str = malloc(2);
+		if (!str)
+			return (NULL);
+		str[0] = c;
+		str[1] = '\0';
+		return (str);
+	}
+	s1_len = ft_strlen(s1);
+	str = malloc(s1_len + 2);
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = c;
+	str[i + 1] = '\0';
+	free(s1);
+	return (str);
+}
+
+size_t	ft_strlcpy(char **dest, char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_size;
+
+	i = 0;
+	if (!src)
+		return (0);
+	src_size = ft_strlen(src);
+	if (size > 0)
+	{
+		while (i < (size - 1) && src[i])
+		{
+			(*dest)[i] = src[i];
+			i++;
+		}
+		(*dest)[i] = '\0';
+	}
+	return (src_size);
+}
